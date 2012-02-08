0b574f17361a55872e2fd82e411a2d5038ebe3f7
Duncan McGreggor <duncan@dreamhost.com>
2012-02-07 20:06:02 -0800

    Added a meta data file.

---
+++ dreamstack/meta.py
@@ -0,0 +1,10 @@
+display_name = "DreamStack-Deploy"
+library_name = "dreamstack"
+author = "Duncan McGreggor"
+author_email = "duncan@dreamhost.com"
+license = "BSD"
+url = "https://github.com/dreamhost/dreamstack-deploy"
+description = """
+DreamHost deploys its cloud stack using the Python-based Fabric remote (and
+local) deployment tool. The code in this project details how that gets done.
+"""
