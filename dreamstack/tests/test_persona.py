0e1ad2ad42f5a75e9b6916fe01a99c457bbaec0d
Duncan McGreggor <duncan@dreamhost.com>
2012-02-08 16:11:05 -0800

    Added new unit test module.

---
+++ dreamstack/tests/test_persona.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+
+from dreamstack import persona
+
+
+class PersonaTestCase(TestCase):
+    """
+    """
+    def setUp(self):
+        self.persona = persona.Persona()
+
+    def test_creation(self):
+        self.assertEqual(self.persona._main_package.uri, "git://uri")
+        self.assertEqual(self.persona._main_package.install_path, "./")
+        self.assertEqual(self.persona._main_package.upstream.uri, "git://uri")
+        
